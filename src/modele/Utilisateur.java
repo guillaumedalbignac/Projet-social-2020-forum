bf15bd34619d812fc732e9934cad73485945fcf6
Alexandre Roussel <alexandreroussel.fr@gmail.com>
2020-12-16 14:05:07 -0500

    modele Utilisateur et son DAO

---
+++ src/modele/Utilisateur.java
@@ -0,0 +1,49 @@
+package modele;
+
+public class Utilisateur {
+	
+	protected String email;
+	protected String pseudo;
+	protected String bio;
+	protected int age;
+
+	public Utlisateur(String email, String pseudo, String bio, int age) {
+		this.email = email;
+		this.pseudo = pseudo;
+		this.bio = bio;
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPseudo() {
+		return this.pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getBio() {
+		return this.bio;
+	}
+
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
