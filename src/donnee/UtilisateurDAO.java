65240f925bac7f6f57a4942ecc500c0fa3479366
guillaumedalbignac <dalbignacguillaume@gmail.com>
2021-02-15 17:24:47 -0500

    #1 Rajout de la table utilisateurs pour le bon fonctionnement
    - Ajout en local de la table utilisateur car inexistante auparavant
    - Corrections du nom de certains id incorrects dans profil.fxml
    - Ajout du fichier SQl au complet

--- src/donnee/UtilisateurDAO.java
+++ src/donnee/UtilisateurDAO.java
@@ -27,13 +27,15 @@ public class UtilisateurDAO {
 		try {
 			requeteUtilisateurs = connection.prepareStatement("SELECT * FROM utilisateur");
 			ResultSet curseurListeUtilisateurs = requeteUtilisateurs.executeQuery();
+			System.out.println("Query : " + curseurListeUtilisateurs);
 			
 			while(curseurListeUtilisateurs.next())
 			{
-				String email = curseurListeUtilisateurs.getString("email");
-				String pseudo = curseurListeUtilisateurs.getString("pseudo");
+				//String pseudo = curseurListeUtilisateurs.getString("pseudo");
+				String pseudo = "test";
+				int age = curseurListeUtilisateurs.getInt("age");							
 				String bio = curseurListeUtilisateurs.getString("bio");
-				int age = curseurListeUtilisateurs.getInt("age");
+				String email = curseurListeUtilisateurs.getString("email");	
 
 				listeUtilisateurs.add(new Utilisateur(email, pseudo, bio, age));
 			}
